e0cfd4b30acf4d7869758ac939f53d85a465831f
aprkh <68245961+aprkh@users.noreply.github.com>
2022-02-14 18:00:01 -0500

    Fixed bug present when irrelevant sets are included in the list of sets

--- set_cover_greedy.py
+++ set_cover_greedy.py
@@ -11,6 +11,9 @@ from data_structures.bucket_queue.bucket_queue import BucketQueue
 import sys
 import random
 
+MIN_ELEMENT = 0
+MAX_ELEMENT = int(10e6)
+
 
 def main():
     elements = set([1, 3, 5])
@@ -20,8 +23,8 @@ def main():
     elements = set([1, 3, 5, 6, 7, 8, 9, 10, 21, 48, 59, 62, 71, 80, 99, 100])
     set1 = set([1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 21, 48])
     set2 = set([59, 62, 71, 80, 81, 99])
-    # set3 = set([101, 102])
-    sets = [set1, set2]
+    set3 = set([101, 102])
+    sets = [set1, set2, set3]
     set_cover_greedy(sets, elements)
 
 
@@ -55,11 +58,13 @@ def set_cover_greedy(sets, elements):
         for ele in s:
             if ele in elements:
                 priority += 1
-        bq.insert(i, priority)
-        sets[i] = (s, priority)
+        # don't include sets with no relevant elements.
+        if priority > 0:
+            bq.insert(i, priority)
+            sets[i] = (s, priority)
 
-    # Unused sets
-    uncovered = set([i for i in range(len(sets))])
+    # Unused sets - only include sets that intersect with elements.
+    uncovered = set([i for i, x in enumerate(sets) if x[1]])
 
     while n_false > 0:
         # make sure at least one element is covered each round, otherwise break
@@ -107,9 +112,9 @@ def set_cover_greedy(sets, elements):
     return cover
 
 
-def randomized_test(ntests, nmin_elements=50, nmax_elements=int(10e6),
-                    nmin_sets=1, nmax_sets=1000, noise=False, cover_all=True,
-                    algorithm=set_cover_greedy, **kwargs):
+def run_randomized_tests(ntests, nmin_elements=50, nmax_elements=int(10e6),
+                         nmin_sets=1, nmax_sets=1000, noise=False, cover_all=True,
+                         algorithm=set_cover_greedy, **kwargs):
     """
     Creates a random set of elements in the range [nmin_elements, nmax_elements] 
     to be covered. Randomly generates sets so that the union of these sets covers 
@@ -119,6 +124,49 @@ def randomized_test(ntests, nmin_elements=50, nmax_elements=int(10e6),
 
     Returns a boolean value indicating whether or not all of the elements are 
     covered by the returned greedy algorithm cover.  
+    """
+    assert nmin_sets >= 1, "Error: must have at least one set!"
+
+
+def single_randomized_test(nmin_elements, nmax_elements, nmin_sets, nmax_sets,
+                           noise, cover_all, algorithm, **kwargs):
+    """
+    Creates a random set of elements in the range [nmin_elements, nmax_elements] 
+    to be covered. Randomly generates sets so that the union of these sets covers 
+    all or a subset of these elements (depending on boolean value of cover_all).
+
+    If noise is True, some elements not needing to be covered will be added to 
+    the sets.
+
+    Returns a boolean value indicating whether or not all of the elements are 
+    covered by the returned greedy algorithm cover.  
+    """
+    nsets = random.randint(nmin_sets, nmax_sets)
+    nelements = random.randint(nmin_elements, nmax_elements)
+
+    set_of_all_elements = set()
+    for _ in range(nelements):
+        set_of_all_elements.add(random.randint(MIN_ELEMENT, MAX_ELEMENT))
+
+    sets = generate_sets(set_of_all_elements, nsets, noise=False)
+
+    cover = algorithm(sets, set_of_all_elements)
+
+    set_of_all_elements = {x: False for x in set_of_all_elements}
+    ct = len(set_of_all_elements)
+    for included_set in cover:
+        for x in included_set:
+            if set_of_all_elements.get(x, False):
+                ct -= 1
+                set_of_all_elements[x] = True
+
+    return ct == 0
+
+
+def generate_sets(universe, nsets, noise):
+    """
+    Randomly generates nsets sets from a universe of elements, such that the 
+    union of the sets is equal to the universe. 
     """
     raise NotImplementedError
 
